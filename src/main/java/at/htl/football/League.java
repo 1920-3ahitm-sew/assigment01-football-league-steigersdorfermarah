dfb1e7502a6fc15e3f77b41f66cdc94bafe81d23
steigersdorfermarah <madschi.steigersdorfer@gmx.at>
2019-09-19 22:51:49 +0200

    zwischen commit

--- src/main/java/at/htl/football/League.java
+++ src/main/java/at/htl/football/League.java
@@ -25,7 +25,6 @@ public class League {
             if (teams.get(i).getName().compareTo(team) == 0) {
                 return teams.get(i);
             }
-
         }
 
         Team team1 = new Team(team);
@@ -36,18 +35,14 @@ public class League {
     public List<Team> getTable(){
         Collections.sort(teams);
 
-        String.format("Team             Pts     W       D       L       GF      GA      GD");
+        System.out.println("Team                 Pts  W   D   L   GF  GA  GD");
         System.out.println("");
+
         for (int i = 0; i < teams.size(); i++) {
-            System.out.printf(teams.get(i).getName() + "               " + teams.get(i).getPoints() + "   " + teams.get(i).getDraws()+ " " + teams.get(i).getDefeats() + " " + teams.get(i).getGoalsShot()+ " " + teams.get(i).getGoalsReceived() + "%n");
+
+            System.out.printf(teams.get(i).getName() + " \t\t\t   " + teams.get(i).getPoints() + "\t" + teams.get(i).getDraws()+ "\t" + teams.get(i).getDefeats() + "\t" + teams.get(i).getGoalsShot()+ "\t" + teams.get(i).getGoalsReceived()  + "%n");
         }
 
         return null;
     }
-
-
-   /* @Override
-    public String toString() {
-        return String.format("%s                %d   %d   %d   %d   %d   %d   %d", team.getName(), team.getPoints(), team.getWins(),team.getDraws(), team.getDefeats(),team.getGoalsShot(),team.getGoalsReceived());
-    }*/
 }
